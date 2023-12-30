3d45a3a27476e6347d61549bc70016bb401d4c32
GuiMedeirox <omaewa9111@gmail.com>
2023-12-30 15:33:11 -0300

    trabalhando com arraylist

--- src/BarrayListWithObjects/Aluno.java
+++ src/BarrayListWithObjects/Aluno.java
@@ -19,8 +19,15 @@ public class Aluno implements Comparable<Aluno>{
         return "[ Aluno: " +this.nome +", idade: " +this.idade +"]";
     }
 
+    /*
+    * A funcao compareTo() funciona para comparacao entre Strings, assim sendo, nao conseguimos comparar objetos do tipo Aluno, por exemplo.
+    *
+    * Logo, para que consigamos realizar essa comparacao, precisamos sobrescrever a funcao de forma que consigamos comparar algum atributo da classe
+    *
+    * */
     @Override
     public int compareTo(Aluno aluno) {
         return this.nome.compareTo(aluno.nome);
     }
+
 }
